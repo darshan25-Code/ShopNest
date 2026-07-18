9025112bbae359d221234b3a4ff95d1bfa668d20
Darshan Raiyani <darshanraiyani63@gmail.com>
2026-07-18 12:38:18 +0530

    Prepare frontend for production

--- server/server.js
+++ server/server.js
@@ -11,7 +11,15 @@ const paymentRoutes = require("./routes/paymentRoutes");
 const app = express()
 const PORT = process.env.PORT || 5000 
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://your-vercel-app.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json())
 
 app.use("/api/products",productRoutes)
