03436440c1b160d924782766d833b2a7378eb6f8
Darshan Raiyani <darshanraiyani63@gmail.com>
2026-07-18 12:44:43 +0530

    Secure production CORS

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-vercel-app.vercel.app",
+      "https://shop-nest-rho.vercel.app",
     ],
     credentials: true,
   })
